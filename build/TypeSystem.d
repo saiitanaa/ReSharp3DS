24f52503bc9b73c55746f47140067af62a414bf1
Saysaa <112180175+saysaa@users.noreply.github.com>
2026-06-01 18:34:26 +0200

    Add files via upload

---
+++ build/TypeSystem.d
@@ -0,0 +1,114 @@
+TypeSystem.o: \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/source/clr/TypeSystem.cpp \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/stdafx.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/source/clr/Core.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Runtime.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Types.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_PlatformDef.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/target_platform.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/target_common.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Types.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nf_errors_exceptions.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Headers.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoWeak.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoPackStruct.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_v2.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Capabilites.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/target_os.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Network.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Power.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_ConfigurationManager.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Boot.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoVersion.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Watchdog.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Rtos.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Windows_Storage.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/Target_Windows_Storage.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Time.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/targetHAL_Time.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/targetHAL.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_ReleaseInfo.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoPAL_Network.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoPAL.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/CPU_GPIO_decl.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Spi.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/TargetHAL_Spi.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/CPU_SPI_decl.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/opcode.def \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Interop.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_ErrorCodes.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoSupport.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Runtime__HeapBlock.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Runtime__Serialization.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Debugging.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Messaging.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoVersion.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol_MonitorCommands.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol_Message.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol_App_Interface.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol_HAL_Interface.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Profiling.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Application.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Checks.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Hardware.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/corlib_native.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/source/clr/corhdr_private.h
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/stdafx.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/source/clr/Core.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Runtime.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Types.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_PlatformDef.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/target_platform.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/target_common.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Types.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nf_errors_exceptions.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Headers.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoWeak.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoPackStruct.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_v2.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Capabilites.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/target_os.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Network.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Power.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_ConfigurationManager.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Boot.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoVersion.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Watchdog.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Rtos.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Windows_Storage.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/Target_Windows_Storage.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Time.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/targetHAL_Time.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/targetHAL.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_ReleaseInfo.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoPAL_Network.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoPAL.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/CPU_GPIO_decl.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Spi.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/TargetHAL_Spi.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/CPU_SPI_decl.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/opcode.def:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Interop.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_ErrorCodes.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoSupport.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Runtime__HeapBlock.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Runtime__Serialization.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Debugging.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Messaging.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoVersion.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol_MonitorCommands.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol_Message.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol_App_Interface.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol_HAL_Interface.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Profiling.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Application.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Checks.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Hardware.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/corlib_native.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/source/clr/corhdr_private.h:
---
+++ build/main.d
@@ -0,0 +1,275 @@
+main.o: C:/Users/l-primoo/Desktop/ReSharp3DS/source/main.cpp \
+ C:/devkitPro/libctru/include/3ds.h \
+ C:/devkitPro/libctru/include/3ds/types.h \
+ C:/devkitPro/libctru/include/3ds/result.h \
+ C:/devkitPro/libctru/include/3ds/ipc.h \
+ C:/devkitPro/libctru/include/3ds/svc.h \
+ C:/devkitPro/libctru/include/3ds/exheader.h \
+ C:/devkitPro/libctru/include/3ds/srv.h \
+ C:/devkitPro/libctru/include/3ds/errf.h \
+ C:/devkitPro/libctru/include/3ds/os.h \
+ C:/devkitPro/libctru/include/3ds/synchronization.h \
+ C:/devkitPro/libctru/include/3ds/thread.h \
+ C:/devkitPro/libctru/include/3ds/gfx.h \
+ C:/devkitPro/libctru/include/3ds/services/gspgpu.h \
+ C:/devkitPro/libctru/include/3ds/console.h \
+ C:/devkitPro/libctru/include/3ds/env.h \
+ C:/devkitPro/libctru/include/3ds/util/decompress.h \
+ C:/devkitPro/libctru/include/3ds/util/utf.h \
+ C:/devkitPro/libctru/include/3ds/allocator/linear.h \
+ C:/devkitPro/libctru/include/3ds/allocator/mappable.h \
+ C:/devkitPro/libctru/include/3ds/allocator/vram.h \
+ C:/devkitPro/libctru/include/3ds/services/ac.h \
+ C:/devkitPro/libctru/include/3ds/services/act.h \
+ C:/devkitPro/libctru/include/3ds/services/frd.h \
+ C:/devkitPro/libctru/include/3ds/mii.h \
+ C:/devkitPro/libctru/include/3ds/services/am.h \
+ C:/devkitPro/libctru/include/3ds/services/fs.h \
+ C:/devkitPro/libctru/include/3ds/services/ampxi.h \
+ C:/devkitPro/libctru/include/3ds/services/apt.h \
+ C:/devkitPro/libctru/include/3ds/services/boss.h \
+ C:/devkitPro/libctru/include/3ds/services/cam.h \
+ C:/devkitPro/libctru/include/3ds/services/y2r.h \
+ C:/devkitPro/libctru/include/3ds/services/cfgnor.h \
+ C:/devkitPro/libctru/include/3ds/services/cfgu.h \
+ C:/devkitPro/libctru/include/3ds/services/csnd.h \
+ C:/devkitPro/libctru/include/3ds/services/dsp.h \
+ C:/devkitPro/libctru/include/3ds/services/fspxi.h \
+ C:/devkitPro/libctru/include/3ds/services/fsreg.h \
+ C:/devkitPro/libctru/include/3ds/services/gsplcd.h \
+ C:/devkitPro/libctru/include/3ds/services/hid.h \
+ C:/devkitPro/libctru/include/3ds/services/irrst.h \
+ C:/devkitPro/libctru/include/3ds/services/sslc.h \
+ C:/devkitPro/libctru/include/3ds/services/httpc.h \
+ C:/devkitPro/libctru/include/3ds/services/uds.h \
+ C:/devkitPro/libctru/include/3ds/services/ndm.h \
+ C:/devkitPro/libctru/include/3ds/services/nim.h \
+ C:/devkitPro/libctru/include/3ds/services/nwmext.h \
+ C:/devkitPro/libctru/include/3ds/services/ir.h \
+ C:/devkitPro/libctru/include/3ds/services/ns.h \
+ C:/devkitPro/libctru/include/3ds/services/pmapp.h \
+ C:/devkitPro/libctru/include/3ds/services/pmdbg.h \
+ C:/devkitPro/libctru/include/3ds/services/ps.h \
+ C:/devkitPro/libctru/include/3ds/services/ptmu.h \
+ C:/devkitPro/libctru/include/3ds/services/ptmsysm.h \
+ C:/devkitPro/libctru/include/3ds/services/ptmgets.h \
+ C:/devkitPro/libctru/include/3ds/services/ptmsets.h \
+ C:/devkitPro/libctru/include/3ds/services/ptmplays.h \
+ C:/devkitPro/libctru/include/3ds/services/pxidev.h \
+ C:/devkitPro/libctru/include/3ds/services/pxipm.h \
+ C:/devkitPro/libctru/include/3ds/services/soc.h \
+ C:/devkitPro/libctru/include/netinet/in.h \
+ C:/devkitPro/libctru/include/sys/socket.h \
+ C:/devkitPro/libctru/include/3ds/services/mic.h \
+ C:/devkitPro/libctru/include/3ds/services/mvd.h \
+ C:/devkitPro/libctru/include/3ds/services/nfc.h \
+ C:/devkitPro/libctru/include/3ds/services/news.h \
+ C:/devkitPro/libctru/include/3ds/services/qtm.h \
+ C:/devkitPro/libctru/include/3ds/services/qtmc.h \
+ C:/devkitPro/libctru/include/3ds/services/srvpm.h \
+ C:/devkitPro/libctru/include/3ds/services/loader.h \
+ C:/devkitPro/libctru/include/3ds/services/mcuhwc.h \
+ C:/devkitPro/libctru/include/3ds/services/cdcchk.h \
+ C:/devkitPro/libctru/include/3ds/gpu/gx.h \
+ C:/devkitPro/libctru/include/3ds/gpu/gpu.h \
+ C:/devkitPro/libctru/include/3ds/gpu/registers.h \
+ C:/devkitPro/libctru/include/3ds/gpu/enums.h \
+ C:/devkitPro/libctru/include/3ds/gpu/shbin.h \
+ C:/devkitPro/libctru/include/3ds/gpu/shaderProgram.h \
+ C:/devkitPro/libctru/include/3ds/ndsp/ndsp.h \
+ C:/devkitPro/libctru/include/3ds/ndsp/channel.h \
+ C:/devkitPro/libctru/include/3ds/applets/swkbd.h \
+ C:/devkitPro/libctru/include/3ds/applets/error.h \
+ C:/devkitPro/libctru/include/3ds/applets/miiselector.h \
+ C:/devkitPro/libctru/include/3ds/archive.h \
+ C:/devkitPro/libctru/include/3ds/romfs.h \
+ C:/devkitPro/libctru/include/3ds/font.h \
+ C:/devkitPro/libctru/include/3ds/gdbhio_dev.h \
+ C:/devkitPro/libctru/include/3ds/3dslink.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Types.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_PlatformDef.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/target_platform.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/target_common.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Types.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nf_errors_exceptions.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Headers.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoWeak.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoPackStruct.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_v2.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Capabilites.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/target_os.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Network.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Power.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_ConfigurationManager.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Boot.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoVersion.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Watchdog.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Rtos.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Windows_Storage.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/Target_Windows_Storage.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Time.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/targetHAL_Time.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/targetHAL.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_ReleaseInfo.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoPAL_Network.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoPAL.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/CPU_GPIO_decl.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Spi.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/TargetHAL_Spi.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/CPU_SPI_decl.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/opcode.def \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Runtime.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Interop.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_ErrorCodes.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoSupport.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Runtime__HeapBlock.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Runtime__Serialization.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Debugging.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Messaging.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoVersion.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol_MonitorCommands.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol_Message.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol_App_Interface.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol_HAL_Interface.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Profiling.h \
+ C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Application.h
+C:/devkitPro/libctru/include/3ds.h:
+C:/devkitPro/libctru/include/3ds/types.h:
+C:/devkitPro/libctru/include/3ds/result.h:
+C:/devkitPro/libctru/include/3ds/ipc.h:
+C:/devkitPro/libctru/include/3ds/svc.h:
+C:/devkitPro/libctru/include/3ds/exheader.h:
+C:/devkitPro/libctru/include/3ds/srv.h:
+C:/devkitPro/libctru/include/3ds/errf.h:
+C:/devkitPro/libctru/include/3ds/os.h:
+C:/devkitPro/libctru/include/3ds/synchronization.h:
+C:/devkitPro/libctru/include/3ds/thread.h:
+C:/devkitPro/libctru/include/3ds/gfx.h:
+C:/devkitPro/libctru/include/3ds/services/gspgpu.h:
+C:/devkitPro/libctru/include/3ds/console.h:
+C:/devkitPro/libctru/include/3ds/env.h:
+C:/devkitPro/libctru/include/3ds/util/decompress.h:
+C:/devkitPro/libctru/include/3ds/util/utf.h:
+C:/devkitPro/libctru/include/3ds/allocator/linear.h:
+C:/devkitPro/libctru/include/3ds/allocator/mappable.h:
+C:/devkitPro/libctru/include/3ds/allocator/vram.h:
+C:/devkitPro/libctru/include/3ds/services/ac.h:
+C:/devkitPro/libctru/include/3ds/services/act.h:
+C:/devkitPro/libctru/include/3ds/services/frd.h:
+C:/devkitPro/libctru/include/3ds/mii.h:
+C:/devkitPro/libctru/include/3ds/services/am.h:
+C:/devkitPro/libctru/include/3ds/services/fs.h:
+C:/devkitPro/libctru/include/3ds/services/ampxi.h:
+C:/devkitPro/libctru/include/3ds/services/apt.h:
+C:/devkitPro/libctru/include/3ds/services/boss.h:
+C:/devkitPro/libctru/include/3ds/services/cam.h:
+C:/devkitPro/libctru/include/3ds/services/y2r.h:
+C:/devkitPro/libctru/include/3ds/services/cfgnor.h:
+C:/devkitPro/libctru/include/3ds/services/cfgu.h:
+C:/devkitPro/libctru/include/3ds/services/csnd.h:
+C:/devkitPro/libctru/include/3ds/services/dsp.h:
+C:/devkitPro/libctru/include/3ds/services/fspxi.h:
+C:/devkitPro/libctru/include/3ds/services/fsreg.h:
+C:/devkitPro/libctru/include/3ds/services/gsplcd.h:
+C:/devkitPro/libctru/include/3ds/services/hid.h:
+C:/devkitPro/libctru/include/3ds/services/irrst.h:
+C:/devkitPro/libctru/include/3ds/services/sslc.h:
+C:/devkitPro/libctru/include/3ds/services/httpc.h:
+C:/devkitPro/libctru/include/3ds/services/uds.h:
+C:/devkitPro/libctru/include/3ds/services/ndm.h:
+C:/devkitPro/libctru/include/3ds/services/nim.h:
+C:/devkitPro/libctru/include/3ds/services/nwmext.h:
+C:/devkitPro/libctru/include/3ds/services/ir.h:
+C:/devkitPro/libctru/include/3ds/services/ns.h:
+C:/devkitPro/libctru/include/3ds/services/pmapp.h:
+C:/devkitPro/libctru/include/3ds/services/pmdbg.h:
+C:/devkitPro/libctru/include/3ds/services/ps.h:
+C:/devkitPro/libctru/include/3ds/services/ptmu.h:
+C:/devkitPro/libctru/include/3ds/services/ptmsysm.h:
+C:/devkitPro/libctru/include/3ds/services/ptmgets.h:
+C:/devkitPro/libctru/include/3ds/services/ptmsets.h:
+C:/devkitPro/libctru/include/3ds/services/ptmplays.h:
+C:/devkitPro/libctru/include/3ds/services/pxidev.h:
+C:/devkitPro/libctru/include/3ds/services/pxipm.h:
+C:/devkitPro/libctru/include/3ds/services/soc.h:
+C:/devkitPro/libctru/include/netinet/in.h:
+C:/devkitPro/libctru/include/sys/socket.h:
+C:/devkitPro/libctru/include/3ds/services/mic.h:
+C:/devkitPro/libctru/include/3ds/services/mvd.h:
+C:/devkitPro/libctru/include/3ds/services/nfc.h:
+C:/devkitPro/libctru/include/3ds/services/news.h:
+C:/devkitPro/libctru/include/3ds/services/qtm.h:
+C:/devkitPro/libctru/include/3ds/services/qtmc.h:
+C:/devkitPro/libctru/include/3ds/services/srvpm.h:
+C:/devkitPro/libctru/include/3ds/services/loader.h:
+C:/devkitPro/libctru/include/3ds/services/mcuhwc.h:
+C:/devkitPro/libctru/include/3ds/services/cdcchk.h:
+C:/devkitPro/libctru/include/3ds/gpu/gx.h:
+C:/devkitPro/libctru/include/3ds/gpu/gpu.h:
+C:/devkitPro/libctru/include/3ds/gpu/registers.h:
+C:/devkitPro/libctru/include/3ds/gpu/enums.h:
+C:/devkitPro/libctru/include/3ds/gpu/shbin.h:
+C:/devkitPro/libctru/include/3ds/gpu/shaderProgram.h:
+C:/devkitPro/libctru/include/3ds/ndsp/ndsp.h:
+C:/devkitPro/libctru/include/3ds/ndsp/channel.h:
+C:/devkitPro/libctru/include/3ds/applets/swkbd.h:
+C:/devkitPro/libctru/include/3ds/applets/error.h:
+C:/devkitPro/libctru/include/3ds/applets/miiselector.h:
+C:/devkitPro/libctru/include/3ds/archive.h:
+C:/devkitPro/libctru/include/3ds/romfs.h:
+C:/devkitPro/libctru/include/3ds/font.h:
+C:/devkitPro/libctru/include/3ds/gdbhio_dev.h:
+C:/devkitPro/libctru/include/3ds/3dslink.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Types.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_PlatformDef.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/target_platform.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/target_common.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Types.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nf_errors_exceptions.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Headers.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoWeak.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoPackStruct.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_v2.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Capabilites.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/target_os.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Network.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Power.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_ConfigurationManager.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Boot.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoVersion.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Watchdog.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Rtos.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Windows_Storage.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/Target_Windows_Storage.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Time.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/targetHAL_Time.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/targetHAL.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_ReleaseInfo.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoPAL_Network.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoPAL.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/CPU_GPIO_decl.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoHAL_Spi.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/TargetHAL_Spi.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/CPU_SPI_decl.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/opcode.def:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Runtime.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Interop.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_ErrorCodes.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoSupport.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Runtime__HeapBlock.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Runtime__Serialization.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Debugging.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Messaging.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoVersion.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol_MonitorCommands.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol_Message.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol_App_Interface.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/WireProtocol_HAL_Interface.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Profiling.h:
+C:/Users/l-primoo/Desktop/ReSharp3DS/include/nanoCLR_Application.h:
